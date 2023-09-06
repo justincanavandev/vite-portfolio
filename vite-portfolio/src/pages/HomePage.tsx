455b14b337dc3d304a026fe25a007d94da001f79
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-09-06 11:20:49 -0400

    bubble animation working, set it up in tailwind config next

--- vite-portfolio/src/pages/HomePage.tsx
+++ vite-portfolio/src/pages/HomePage.tsx
@@ -1,21 +1,23 @@
 import { NavLink } from "react-router-dom"
+import { useContext } from "react"
 import "../homepage.css"
+import { PortfolioContext } from "../context/PortfolioContext"
 
+function HomePage() {
+  const { componentHeight } = useContext(PortfolioContext)
 
-function HomePage () {
+  return (
+    <div style={{ height: componentHeight }} className="ml-5">
+      <span className="welcome">Welcome! </span>
 
-    return (
-        <div>
-            <span className="welcome">Welcome! </span>
-
-            <p className="click-the-bubble">Click the BUBBLE to learn more!</p>
-
-            {/* <NavLink to="/about-me">CLICK ME</NavLink> */}
-            
+      <p className="click-the-bubble">Want to learn more?</p>
+      <NavLink className="" to="/about-me">
+        <div className="bubble-div">
+          <div className="bubble w-48 h-32 mt-5 text-center relative text-white"></div>
         </div>
-
-    )
-
+      </NavLink>
+    </div>
+  )
 }
 
 export default HomePage
