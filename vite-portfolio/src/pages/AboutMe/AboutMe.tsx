42a2e553694f473d6a22237437b361919decfe6d
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-03-20 14:49:07 -0500

    set up database and orms icon section

--- vite-portfolio/src/pages/AboutMe/AboutMe.tsx
+++ vite-portfolio/src/pages/AboutMe/AboutMe.tsx
@@ -8,8 +8,7 @@ import { GlobalContext } from "../../context/GlobalContext"
 import Employment from "./Employment"
 
 export default function AboutMe() {
-  const { screenWidth, isModalOpen, openOrClose } =
-    useContext(GlobalContext)
+  const { screenWidth, isModalOpen, openOrClose } = useContext(GlobalContext)
 
   const languagesAndTools: Skill[] = [
     {
@@ -35,21 +34,26 @@ export default function AboutMe() {
       icon: "devicon:python",
     },
     {
-      title: "JQuery",
-      icon: "devicon:jquery",
+      title: "GraphQL",
+      icon: "akar-icons:graphql-fill",
     },
+
+    // {
+    //   title: "JQuery",
+    //   icon: "devicon:jquery",
+    // },
     {
       title: "HTML5",
       icon: "devicon:html5",
     },
-    {
-      title: "Babel",
-      icon: "devicon:babel",
-    },
-    {
-      title: "Github",
-      icon: "bi:github",
-    },
+    // {
+    //   title: "Babel",
+    //   icon: "devicon:babel",
+    // },
+    // {
+    //   title: "Github",
+    //   icon: "bi:github",
+    // },
   ]
 
   const librariesAndFrameworks: Skill[] = [
@@ -58,21 +62,69 @@ export default function AboutMe() {
       icon: "logos:react",
     },
     {
-      title: "TailwindCSS",
-      icon: "devicon:tailwindcss",
-    },
-    {
-      title: "Express.js",
-      icon: "simple-icons:express",
+      title: "Next.js",
+      icon: "teenyicons:nextjs-solid",
     },
     {
       title: "React Query",
       icon: "logos:react-query-icon",
     },
     {
+      title: "tRPC",
+      icon: "devicon-plain:trpc",
+    },
+    {
+      title: "Express.js",
+      icon: "simple-icons:express",
+    },
+    {
+      title: "React Router",
+      icon: "devicon-plain:reactrouter",
+    },
+    {
+      title: "TailwindCSS",
+      icon: "devicon:tailwindcss",
+    },
+    {
       title: "Handlebars.js",
       icon: "vscode-icons:file-type-handlebars",
     },
+  ]
+
+  const databases: Skill[] = [
+    {
+      title: "PostgreSQL",
+      icon: "logos:postgresql",
+    },
+    {
+      title: "Prisma",
+      icon: "file-icons:prisma",
+    },
+    {
+      title: "SQLite",
+      icon: "file-icons:sqlite",
+    },
+    {
+      title: "SQLAlchemy",
+      icon: "simple-icons:sqlalchemy",
+    },
+    {
+      title: "MySQL",
+      icon: "devicon:mysql",
+    },
+    {
+      title: "Sequelize",
+      icon: "devicon:sequelize",
+    },
+    {
+      title: "MongoDB",
+      icon: "devicon:mongodb",
+    },
+    {
+      title: "Mongoose",
+      icon: "simple-icons:mongoose",
+    },
+
   ]
 
   const [displayIconTitle, setDisplayIconTitle] = useState<boolean>(false)
@@ -167,7 +219,7 @@ export default function AboutMe() {
   return (
     <>
       <div
-      id="about-me"
+        id="about-me"
         className={`w-full bg-black mb-4 mt-4 font-oswald flex flex-col lg:max-w-screen  text-white ${overflowClass}   ${
           isModalOpen
             ? openOrClose
@@ -189,17 +241,18 @@ export default function AboutMe() {
               About Me
             </h2>
             <div className="lg:flex lg:justify-between">
-              <p className="text-[1.1rem] px-5 xs:pl-5 xs:pr-[30%] sm:text-[1.3rem] sm:pr-8 md:px-5 md:max-w-[48%] lg:text-[1.7rem] lg:w-[46%]">
-                A Full-Stack Developer specializing in technologies ranging
-                from TypeScript, React, Next.js, Node.js, T3, Express.js, GraphQL, API construction, Python, and more. 
+              <p className="text-[1.1rem] px-5 xs:pl-5 xs:pr-[30%] sm:text-[1.3rem] sm:pr-8 md:px-5 md:max-w-[48%] lg:text-[1.7rem] lg:w-[46%] 2xl:text-[2.2rem]">
+                A Full-Stack Developer specializing in technologies ranging from
+                TypeScript, React, Next.js, Node.js, T3, Express.js, GraphQL,
+                API construction, Python, and more.
                 {/* As a freelance musician for over a decade, my commitment to the art of crafting immersive experiences has been my driving motivation. */}
               </p>
-              <Employment className="hidden lg:flex lg:flex-col lg:h-fit lg:w-[46%] lg:text-[1.8rem] xl:text-[2.2rem]" />
+              <Employment className="hidden lg:flex lg:flex-col lg:h-fit lg:w-[46%] lg:text-[1.8rem] xl:text-[2.2rem] 2xl:text-[2.8rem]" />
             </div>
 
-            <div className="px-2 sm:flex sm:flex-col sm:gap-4 lg:w-screen md:max-w-[550px] lg:flex-row lg:justify-between lg:max-w-none lg:px-6 lg:mt-6">
+            <div className="px-2 sm:flex sm:flex-col sm:gap-4 lg:w-screen md:max-w-[550px] lg:flex-row lg:justify-between lg:max-w-none lg:flex-wrap lg:px-6 lg:mt-6">
               <div className="flex flex-col mt-4 gap-3 sm:gap-5 sm:max-w-[450px] md:max-w-[600px] lg:w-[46%] lg:mt-0">
-                <div className="flex justify-between items-center text-[1.3rem] sm:text-[1.8rem] xl:text-[2.2rem]">
+                <div className="flex justify-between items-center text-[1.3rem] sm:text-[1.8rem] xl:text-[2.2rem] 2xl:text-[2.8rem]">
                   <h3 className="uppercase">Languages and Tools </h3>
                   {displayIconTitle &&
                     containsDisplayedIcon(languagesAndTools) && (
@@ -218,7 +271,7 @@ export default function AboutMe() {
                             icon={tool.icon}
                             onMouseOver={() => iconTitle(tool.title)}
                             onMouseOut={() => setDisplayIconTitle(false)}
-                            className={` relative hover:border p-[1.5px] rounded-md text-[2.3rem] sm:text-[2.7rem] lg:text-[3rem]  xl:text-[3.4rem] md:p-[3px]`}
+                            className={` relative hover:border p-[1.5px] rounded-md text-[2.3rem] sm:text-[2.7rem] lg:text-[3rem]  xl:text-[3.4rem] 2xl:text-[4.5rem] md:p-[3px]`}
                           ></Icon>
                         </div>
                       </div>
@@ -231,7 +284,7 @@ export default function AboutMe() {
               </div>
               <div className="flex-col lg:w-[46%]">
                 <div className="flex flex-col gap-4">
-                  <div className="flex justify-between items-center mt-2 text-[1.3rem] sm:text-[1.8rem] lg:mt-0 xl:text-[2.2rem]">
+                  <div className="flex justify-between items-center mt-2 text-[1.3rem] sm:text-[1.8rem] lg:mt-0 xl:text-[2.2rem] 2xl:text-[2.8rem]">
                     <h3 className="uppercase ">Libraries and Frameworks </h3>
                     {displayIconTitle &&
                       containsDisplayedIcon(librariesAndFrameworks) && (
@@ -249,19 +302,47 @@ export default function AboutMe() {
                               onMouseOver={() => iconTitle(library.title)}
                               onMouseOut={() => setDisplayIconTitle(false)}
                               icon={library.icon}
-                              className={`hover:border rounded-sm text-[2.3rem] p-[1.5px] sm:text-[2.7rem] lg:text-[3rem] xl:text-[3.4rem] md:p-[3px]`}
+                              className={`hover:border rounded-sm text-[2.3rem] p-[1.5px] sm:text-[2.7rem] lg:text-[3rem] xl:text-[3.4rem] 2xl:text-[4.5rem] md:p-[3px]`}
                             ></Icon>
                           </div>
                         </div>
                       </>
                     ))}
-                    <div
-                      onAnimationEnd={handleOverflow}
-                      className={`absolute inset-0 w-full bg-black ${displayIcons()}`}
-                    ></div>
                   </div>
                 </div>
               </div>
+              <div className="flex-col lg:w-[46%]">
+                <div className="flex flex-col gap-4">
+                  <div className="flex justify-between items-center mt-2 text-[1.3rem] sm:text-[1.8rem] lg:mt-0 xl:text-[2.2rem] 2xl:text-[2.8rem]">
+                    <h3 className="uppercase ">Databases and ORMs</h3>
+                    {displayIconTitle && containsDisplayedIcon(databases) && (
+                      <span className="pr-[.8rem] sm:pr-0">
+                        {displayedIcon}
+                      </span>
+                    )}
+                  </div>
+                  <div className="flex opacity-fade-in relative">
+                    {databases.map((database, dbIndex) => (
+                      <>
+                        <div key={dbIndex} className="flex flex-col relative">
+                          <div className="px-1 flex ">
+                            <Icon
+                              onMouseOver={() => iconTitle(database.title)}
+                              onMouseOut={() => setDisplayIconTitle(false)}
+                              icon={database.icon}
+                              className={`hover:border rounded-sm text-[2.3rem] p-[1.5px] sm:text-[2.7rem] md:p-[3px] lg:text-[3rem] xl:text-[3.4rem] 2xl:text-[4.5rem] `}
+                            ></Icon>
+                          </div>
+                        </div>
+                      </>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div
+                onAnimationEnd={handleOverflow}
+                className={`absolute inset-0 w-full bg-black ${displayIcons()}`}
+              ></div>
             </div>
 
             <Employment className="flex flex-col mt-4 text-[1.3rem] sm:text-[1.7rem] sm:w-auto lg:hidden" />
